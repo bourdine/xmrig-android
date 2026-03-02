104c4782c9a98e5a103fe929759b2e7b727457dd
Andrei Bourdine <bourdine@gmail.com>
2026-03-02 07:33:22 +0300

    Delete web directory

--- web/vite.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    server: {
-        headers: {
-            'Cross-Origin-Opener-Policy': 'same-origin',
-            'Cross-Origin-Embedder-Policy': 'require-corp',
-        },
-    },
-};
